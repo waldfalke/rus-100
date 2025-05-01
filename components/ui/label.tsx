f4776cdb4075a48610a67e820aa39153c1f2d339
Eugen Sokolov <stribojich@gmail.com>
2025-05-01 18:59:05 +0300

    feat: Implement label component tokens and CSS variables

--- components/ui/label.tsx
+++ components/ui/label.tsx
@@ -7,7 +7,7 @@ import { cva, type VariantProps } from "class-variance-authority"
 import { cn } from "@/lib/utils"
 
 const labelVariants = cva(
-  "text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+  "text-[var(--component-label-font-size)] font-[var(--component-label-font-weight)] leading-[var(--component-label-line-height)] peer-disabled:cursor-not-allowed peer-disabled:opacity-[var(--component-label-disabled-opacity)]"
 )
 
 const Label = React.forwardRef<
